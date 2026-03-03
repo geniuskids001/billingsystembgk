a059486c107613a50e581bbb594373371f74b9ab
geniuskids001 <direccion@geniuskids.mx>
2026-03-03 09:14:44 -0600

    date strings
    correccion apra mantener formato america city

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const config = {
     waitForConnections: true,
     connectionLimit: 10,
     timezone: "America/Mexico_City",
+    dateStrings: true
   },
   gcs: {
     bucket: process.env.GCS_BUCKET,
